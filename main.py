01d0bbf77299859c3328ffc31eaad1b43d0c0131
KingMiks <Miko.gaddi27@gmail.com>
2026-06-09 11:23:35 -0400

    Added title label to main window

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from tkinter import Tk
+from tkinter import Tk, Label
 
 window = Tk()
 
 window.title("Job Application Tracker")
 window.geometry("800x600")
 
-
+label = Label(window, text="Job Application Tracker")
+label.pack()
 
 
 
