aff93bd6078f9663b128938aebb9efab17232503
KingMiks <Miko.gaddi27@gmail.com>
2026-06-11 11:30:25 -0400

    added error box for empty fields and erased text fields after save

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
 
 # Create a window
-window = Tk()
+window = tk.Tk()
 
 window.title("Job Application Tracker")
 window.minsize(width=800, height=600)
 
-label = Label(window, text="Job Application Tracker")
+label = tk.Label(window, text="Job Application Tracker")
 label.grid(row=0)
 
 
-company_label = Label(window, text="Company:")
+company_label = tk.Label(window, text="Company:")
 company_label.grid(row=2)
 
-role_label = Label(window, text= "Role:")
+role_label = tk.Label(window, text= "Role:")
 role_label.grid(row=4)
 
 next_row = 6
@@ -22,27 +23,34 @@ next_row = 6
 def save_job():
     global next_row
 
-    company = company_entry.get()
-    role = role_entry.get()
+    company = company_entry.get().strip()
+    role = role_entry.get().strip()
 
-    job_label = Label(window, text=f"{company} | {role}")
+    if not company or not role:
+        messagebox.showerror(title="ERROR EMPTY FIELD", 
+                             message="Please enter valid company or role")
+        return 
+
+    company_entry.delete(0, tk.END)
+    role_entry.delete(0, tk.END)
+
+    job_label = tk.Label(window, text=f"{company} | {role}")
     job_label.grid(row=next_row, column=0, columnspan=2)
-
     next_row +=1
 
 # Entries
 
-company_entry = Entry(window, width=30)
+company_entry = tk.Entry(window, width=30)
 company_entry.grid(row=2, column=1)
 
-role_entry = Entry(window, width=30)
+role_entry = tk.Entry(window, width=30)
 role_entry.grid(row=4, column=1)
 
 # Buttons
 
 # Save job
 
-save_job_button = Button(window, text= "Save job", command=save_job)
+save_job_button = tk.Button(window, text= "Save job", command=save_job)
 save_job_button.grid(row=5)
 
 
